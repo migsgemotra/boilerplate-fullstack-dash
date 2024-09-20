b2008a6ec605decfa302e50b0c0bcf0f51206792
migsgemotra <49238193+migsgemotra@users.noreply.github.com>
2024-09-21 02:21:55 +0800

    added create user mutation

--- src/backend/modules/users/mutations/create.ts
+++ src/backend/modules/users/mutations/create.ts
@@ -1,6 +1,10 @@
+import { ApolloError } from 'apollo-server-express'
 import { Context } from '../../../_types/context'
 
 import { User, CreateUserArgs } from 'backend/_types/users'
+
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export default async (_root: undefined, args: CreateUserArgs, context: Context): Promise<User> => {
 
@@ -10,6 +14,22 @@ export default async (_root: undefined, args: CreateUserArgs, context: Context):
     password
   } = args
 
+  // check if user is existing
+
+  const userExistsAlready: User = await context.database.users.findOne({email: args.email})
+
+  // error if user is existing
+
+  if (userExistsAlready) {
+    throw new ApolloError('User already exists')
+  }
+
+  // encrypt password
+
+  // build mongoos model?
+
+  // create JWT to attach to user
+
   const createObject: User = {
     email,
     username,
@@ -17,11 +37,16 @@ export default async (_root: undefined, args: CreateUserArgs, context: Context):
   }
 
   if (email) {
-    createObject.email = email
+    createObject.email = email.toLowerCase()
   }
 
   if (username) {
     createObject.username = username
+  }
+
+  if (password) {
+    const encryptedPassword = await bcrypt.hash(password)
+    createObject.password = encryptedPassword
   }
 
   const res = await context.database.users.insertOne({
@@ -30,5 +55,26 @@ export default async (_root: undefined, args: CreateUserArgs, context: Context):
 
   const user: User = await context.database.users.findOne({ _id: res.insertedId })
 
-  return user
+const jwtOptions = {
+  expiresIn: '24h'
+}
+
+  const token = jwt.sign(
+    {
+      _id: String(user._id)
+    },
+    'TEST_JWT_SECRET',
+    jwtOptions
+  )
+
+  const generateToken = await context.database.users.findOneAndUpdate({
+    _id: user._id
+  },
+  {
+    $set: {
+      token: token
+    }
+  })
+
+  return generateToken.value
 }
