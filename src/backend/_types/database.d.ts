5a96106e078d7a3e89aba2c5ba891e2b50b8851f
migsgemotra <49238193+migsgemotra@users.noreply.github.com>
2024-09-26 17:46:36 +0800

    fixed

--- src/backend/_types/database.d.ts
+++ src/backend/_types/database.d.ts
@@ -1,9 +1,7 @@
 import { Collection } from 'mongodb'
-import { Test } from 'test'
 import { Note } from './notes'
 
 export interface Database {
-  tests: Collection<Test>
   users: Collection<User>
   notes: Collection<Note>
 }
